1f8694b09c58dfaededa3f9064304da151eb487b
tkmn0 <tkmgin0914@gmail.com>
2020-12-26 10:07:56 +0900

    feat: update main

--- chsared/main.go
+++ chsared/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/tkmn0/caress"
+import (
+	"unsafe"
+
+	"github.com/tkmn0/caress"
+)
 
 var noiseReducers []*caress.NoiseReducer
 
@@ -8,6 +12,15 @@ var noiseReducers []*caress.NoiseReducer
 func CreateNoiseReducer(numChannels int32, sampleRate int32, attenuation float64, model string) {
 	nr := caress.NewNoiseReducer(int(numChannels), uint32(sampleRate), attenuation, caress.RnnoiseModel(model))
 	noiseReducers = append(noiseReducers, nr)
+}
+
+//export CreateEncoder
+func CreateEncoder(sampleRate uint32, channels uint16, application int32) unsafe.Pointer {
+	e, err := caress.NewEncoder(sampleRate, channels, application)
+	if err != nil {
+		return nil
+	}
+	return unsafe.Pointer(e)
 }
 
 func main() {}
