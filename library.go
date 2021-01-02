300c13ae620fb2dbc93280930119d609e503b1c4
tkmn0 <tkmgin0914@gmail.com>
2021-01-02 13:27:07 +0900

    refactor: remove library.go

--- library.go
+++
@@ -1,7 +0,0 @@
-package caress
-
-import "github.com/tkmn0/caress/opus"
-
-func PcmSoftClip(pcm []float32, frameSize int32, channels int32, softClipMem []float32) {
-	opus.PcmSoftClip(pcm, frameSize, channels, softClipMem)
-}
