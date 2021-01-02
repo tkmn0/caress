74ae62baf393b4150bbe51b08dc685d1f0cdf34d
tkmn0 <tkmgin0914@gmail.com>
2021-01-02 14:44:10 +0900

    feat: setup creations

--- cshared/main.go
+++ cshared/main.go
@@ -1,5 +1,6 @@
 package main
 
+import "C"
 import (
 	"unsafe"
 
@@ -7,6 +8,15 @@ import (
 )
 
 var noiseReducers []*caress.NoiseReducer
+var encoders []*caress.Encoder
+var decorders []*caress.Decoder
+
+//export Initialize
+func Initialize() {
+	noiseReducers = []*caress.NoiseReducer{}
+	encoders = []*caress.Encoder{}
+	decorders = []*caress.Decoder{}
+}
 
 //export CreateNoiseReducer
 func CreateNoiseReducer(numChannels int32, sampleRate int32, attenuation float64, model string) {
@@ -20,7 +30,18 @@ func CreateEncoder(sampleRate uint32, channels uint16, application int32) unsafe
 	if err != nil {
 		return nil
 	}
+	encoders = append(encoders, e)
 	return unsafe.Pointer(e)
+}
+
+//export CreateDecoder
+func CreateDecoder(sampleRate uint32, channels uint16) unsafe.Pointer {
+	d, err := caress.NewDecoder(sampleRate, channels)
+	if err != nil {
+		return nil
+	}
+	decorders = append(decorders, d)
+	return unsafe.Pointer(d)
 }
 
 func main() {}
