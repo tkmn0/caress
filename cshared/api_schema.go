d0441ae8d16caa1f4a97acd779005bba3347990b
tkmn0 <tkmgin0914@gmail.com>
2021-01-02 20:07:52 +0900

    feat: update schema

--- cshared/api_schema.go
+++ cshared/api_schema.go
@@ -1,13 +1,26 @@
 package main
 
+import "C"
 import (
 	"unsafe"
 )
 
+type Data struct {
+	Ptr    unsafe.Pointer
+	Length uint32
+}
+
+func (d *Data) StringValue() string {
+	return string(d.ByteValue())
+}
+
+func (d *Data) ByteValue() []byte {
+	return C.GoBytes(d.Ptr, C.int(d.Length))
+}
+
 type ApiError struct {
-	Code          byte
-	Message       unsafe.Pointer
-	MessageLength uint32
+	Code byte
+	Data Data
 }
 
 type PointerResult struct {
@@ -15,14 +28,27 @@ type PointerResult struct {
 	ApiError ApiError
 }
 
-type Array struct {
-	Ptr    unsafe.Pointer
-	Length uint32
+type DataResult struct {
+	ResultData Data
+	ApiError   ApiError
 }
 
-type ArrayResult struct {
-	ResultArray Array
-	ApiError    ApiError
+type NoiseReducerConfig struct {
+	NumChannels int32
+	SampleRate  int32
+	Attenuation float64
+	Model       Data
+}
+
+type EncoderConfig struct {
+	SampleRate  uint32
+	Channels    uint16
+	Application int32
+}
+
+type DecoderConfig struct {
+	SampleRate uint32
+	Channels   uint16
 }
 
 func CreateApiError(err error) *ApiError {
@@ -33,9 +59,11 @@ func CreateApiError(err error) *ApiError {
 	} else {
 		b := []byte(err.Error())
 		return &ApiError{
-			Code:          byte(FromErrorToErrorCode(err)),
-			Message:       unsafe.Pointer(&b[0]),
-			MessageLength: uint32(len(b)),
+			Code: byte(FromErrorToErrorCode(err)),
+			Data: Data{
+				Ptr:    unsafe.Pointer(&b[0]),
+				Length: uint32(len(b)),
+			},
 		}
 	}
 }
--- cshared/main.go
+++ cshared/main.go
@@ -20,15 +20,17 @@ func Initialize() {
 }
 
 //export CreateNoiseReducer
-func CreateNoiseReducer(numChannels int32, sampleRate int32, attenuation float64, model string) {
-	nr := caress.NewNoiseReducer(int(numChannels), uint32(sampleRate), attenuation, caress.RnnoiseModel(model))
+func CreateNoiseReducer(config unsafe.Pointer) {
+	c := (*NoiseReducerConfig)(config)
+	nr := caress.NewNoiseReducer(int(c.NumChannels), uint32(c.SampleRate), c.Attenuation, caress.RnnoiseModel(c.Model.StringValue()))
 	noiseReducers = append(noiseReducers, nr)
 }
 
 //export CreateEncoder
-func CreateEncoder(sampleRate uint32, channels uint16, application int32, result unsafe.Pointer) {
+func CreateEncoder(config unsafe.Pointer, result unsafe.Pointer) {
+	c := (*EncoderConfig)(config)
 	r := (*PointerResult)(result)
-	e, err := caress.NewEncoder(sampleRate, channels, application)
+	e, err := caress.NewEncoder(c.SampleRate, c.Channels, c.Application)
 	r.ApiError = *CreateApiError(err)
 	if e != nil {
 		encoders = append(encoders, e)
@@ -37,9 +39,10 @@ func CreateEncoder(sampleRate uint32, channels uint16, application int32, result
 }
 
 //export CreateDecoder
-func CreateDecoder(sampleRate uint32, channels uint16, result unsafe.Pointer) {
+func CreateDecoder(config unsafe.Pointer, result unsafe.Pointer) {
+	c := (*DecoderConfig)(config)
 	r := (*PointerResult)(result)
-	d, err := caress.NewDecoder(sampleRate, channels)
+	d, err := caress.NewDecoder(c.SampleRate, c.Channels)
 	r.ApiError = *CreateApiError(err)
 	if d != nil {
 		decorders = append(decorders, d)
@@ -49,61 +52,61 @@ func CreateDecoder(sampleRate uint32, channels uint16, result unsafe.Pointer) {
 
 //export Encode
 func Encode(ep unsafe.Pointer, input unsafe.Pointer, result unsafe.Pointer) {
-	i := (*Array)(input)
-	r := (*ArrayResult)(result)
+	i := (*Data)(input)
+	r := (*DataResult)(result)
 	e := (*caress.Encoder)(ep)
 	var pcm []int16
 	var buffer []byte
-	arrayToSlice(*i, unsafe.Pointer(&pcm))
-	arrayToSlice(r.ResultArray, unsafe.Pointer(&buffer))
+	DataToSlice(*i, unsafe.Pointer(&pcm))
+	DataToSlice(r.ResultData, unsafe.Pointer(&buffer))
 	l, err := e.Encode(pcm, buffer)
 	r.ApiError = *CreateApiError(err)
-	r.ResultArray.Length = uint32(l)
+	r.ResultData.Length = uint32(l)
 }
 
 //export EncodeFloat
 func EncodeFloat(ep unsafe.Pointer, input unsafe.Pointer, result unsafe.Pointer) {
-	i := (*Array)(input)
-	r := (*ArrayResult)(result)
+	i := (*Data)(input)
+	r := (*DataResult)(result)
 	e := (*caress.Encoder)(ep)
 	var pcm []float32
 	var buffer []byte
-	arrayToSlice(*i, unsafe.Pointer(&pcm))
-	arrayToSlice(r.ResultArray, unsafe.Pointer(&buffer))
+	DataToSlice(*i, unsafe.Pointer(&pcm))
+	DataToSlice(r.ResultData, unsafe.Pointer(&buffer))
 	l, err := e.EncodeFloat(pcm, buffer)
 	r.ApiError = *CreateApiError(err)
-	r.ResultArray.Length = uint32(l)
+	r.ResultData.Length = uint32(l)
 }
 
 //export Decode
 func Decode(dp unsafe.Pointer, fec bool, input unsafe.Pointer, result unsafe.Pointer) {
-	i := (*Array)(input)
-	r := (*ArrayResult)(result)
+	i := (*Data)(input)
+	r := (*DataResult)(result)
 	d := (*caress.Decoder)(dp)
 	var buffer []byte
 	var pcm []int16
-	arrayToSlice(*i, unsafe.Pointer(&buffer))
-	arrayToSlice(r.ResultArray, unsafe.Pointer(&pcm))
+	DataToSlice(*i, unsafe.Pointer(&buffer))
+	DataToSlice(r.ResultData, unsafe.Pointer(&pcm))
 	l, err := d.Decode(buffer, pcm, fec)
 	r.ApiError = *CreateApiError(err)
-	r.ResultArray.Length = uint32(l)
+	r.ResultData.Length = uint32(l)
 }
 
 //export DecodeFloat
 func DecodeFloat(dp unsafe.Pointer, fec bool, input unsafe.Pointer, result unsafe.Pointer) {
-	i := (*Array)(input)
-	r := (*ArrayResult)(result)
+	i := (*Data)(input)
+	r := (*DataResult)(result)
 	d := (*caress.Decoder)(dp)
 	var buffer []byte
 	var pcm []float32
-	arrayToSlice(*i, unsafe.Pointer(&buffer))
-	arrayToSlice(r.ResultArray, unsafe.Pointer(&pcm))
+	DataToSlice(*i, unsafe.Pointer(&buffer))
+	DataToSlice(r.ResultData, unsafe.Pointer(&pcm))
 	l, err := d.DecodeFloat(buffer, pcm, fec)
 	r.ApiError = *CreateApiError(err)
-	r.ResultArray.Length = uint32(l)
+	r.ResultData.Length = uint32(l)
 }
 
-func arrayToSlice(source Array, dist unsafe.Pointer) {
+func DataToSlice(source Data, dist unsafe.Pointer) {
 	slice := (*reflect.SliceHeader)(dist)
 	slice.Len = int(source.Length)
 	slice.Cap = int(source.Length)
